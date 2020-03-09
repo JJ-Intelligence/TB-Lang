faff5f4cca8e1b0017a85f24fe9c88428ab77fb6
Josh Hook <jljh1g18@soton.ac.uk>
2020-03-09 10:30:52 +0000

    Added some Frames to Evaluator.

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -3,18 +3,32 @@ import Parser
 
 -- Environment - A mapping of functions and variables to Closures (which maps an Expression to an Environment).
 type Env = [ (String, Closure) ]
-data Closure = Clo (E, Env) deriving (Eq, Show)
+data Closure = Clo (Expr, Env) deriving (Show)
 
 -- Kontinuation - A stack containing Frames showing what to do.
 type Kon = [ Frame ]
-data Frame = Done deriving (Show)
 
--- State - The current state/configuration of a CEK machine.
+-- Frame - Data structures to be put onto the Kontinuation.
+data BinOpFrame = BinCompOp ExprComp Expr Env -- Frame for a binary comparison operation - e.g. [-] == e2
+                | BinSeqOp Expr Env -- Frame for a binary sequence operation - e.g. [-] ; e2
+                deriving (Show)
+
+data TerOpFrame = TerIfOp Expr (Maybe ExprElif) Env -- Frame for a ternary if statement operation - e.g. if [-] then e1 e2 (e2 is the else/elif)
+                deriving (Show)
+
+data Frame = HBinOp BinOpFrame
+           | BinOpH BinOpFrame
+           | HTerOp TerOpFrame
+           | TerOpH TerOpFrame
+           | Done 
+           deriving (Show)
+
+-- State - The current state/configuration of the CEK machine.
 type State = (Expr, Env, Kon)
 
 -- Evaluation function to take an Expression (Control) and run it on the finite state machine.
 eval :: Expr -> State
-eval exp = step (exp, env, kon)
+eval e = step (e, [], [Done])
 
 -- Step function to move from one State to another.
 step :: State -> State
