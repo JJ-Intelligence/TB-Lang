42ff6cac618b1b506b819257f617e8c6a219c158
Josh Hook <jljh1g18@soton.ac.uk>
2020-03-11 11:51:37 +0000

    Created Environment and Store (heap) data structures and made the addToEnv and updateStore functions.

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -1,13 +1,15 @@
 module Evaluator where
 import Parser
+import qualified Data.Map.Strict as Map
 
 -- Environment - A mapping of functions and variables to Closures (which maps an Expression to an Environment).
-type Env = [ (EnvType, Closure) ]
-data EnvType = EnvFunc String
-             | EnvVar String
-             deriving (Show)
-data Closure = Clo (Expr, Env) 
-             deriving (Show)
+type Address = Int
+data Environment = [ (String, Address) ]
+type Store = Map Address ExprType
+data ExprType = TInt Int
+              | TBool Bool
+              --| TFunc [ (Expr, Expr) ]
+              deriving (Show)
 
 -- Kontinuation - A stack containing Frames showing what to do.
 type Kon = [ Frame ]
@@ -21,28 +23,49 @@ data TerOpFrame = TerIfOp Expr (Maybe ExprElif) Env -- Frame for a ternary if st
                 deriving (Show)
 
 data Frame = HBinOp BinOpFrame
-           | BinOpH BinOpFrame
+           | BinOpH BinOpFrame    
            | HTerOp TerOpFrame
            | TerOpH TerOpFrame
-           | DefVarH String Env
            | Done 
            deriving (Show)
 
--- State - The current state/configuration of the CEK machine.
-type State = (Expr, Env, Kon)
+-- State - The current state/configuration of the CESK machine.
+type State = (Expr, Environment, Store, Kon)
 
 -- Evaluation function to take an Expression (Control) and run it on the finite state machine.
 eval :: Expr -> State
-eval e = step (e, [], [Done])
+eval e = step (e, Env (empty) Nothing, [Done])
 
 -- Step function to move from one State to another.
 step :: State -> State
 
-step (Seq e1 e2, env, kon) = step (e1, env, (HBinOp $ BinSeqOp e2):kon) -- Not sure If you should store the env for e2 - as it should really get the finished env after e1's been evaluated.
+step (Seq e1 e2, env, store, kon) = step (e1, env, store, (HBinOp $ BinSeqOp e2):kon) -- Not sure If you should store the env for e2 - as it should really get the finished env after e1's been evaluated.
 
 -- Defining a new Var - evaluate e1 before adding the new Var to the Env.
-step (DefVar s e1, env, kon) = step (e1, env, (DefVarH s env):kon)
+-- DefVar s e1
+--      => 1) Evaluate e1 and check its type is okay (e.g. 1:True:3:[] should throw an error)
+--      => 2) Check the env and store for the variable:
+--          => 2a) if in the env and store, then update, else create a new variable.
+step (DefVar s e1, env, store, kon) = 
+    | addr == Nothing = step (c, env', updateStore store' addr' c, kon)
+    | otherwise = step (c, env, updateStore store' addr c, kon)
+    where addr = lookup s env
+          (env', addr') = addToEnv env s
+          (c,e,store',k) = step (e1, env, store, kon)
 
 step s@(_, _, [Done]) = s
 
-step (exp, env, kon) = error $ "ERROR evaluating expression " ++ (show exp) ++ ", no CEK step defined."
+step (exp, env, kon) = error $ "ERROR evaluating expression " ++ (show exp) ++ ", no CESK step defined."
+
+-- Adds a new String to the Environment, and returns a tuple of the new Environment and the created Address.
+addToEnv :: Environment -> String -> (Environment, Address)
+addToEnv [] x = ([(x,0)], 0)
+addToEnv ((s,a):env) x = ((x,a+1) : (s,a) : env, a+1)
+
+-- Updates an Address mapping in the store.
+-- If the inputted Address is not in the store, then it will be inserted.
+updateStore :: Store -> Address -> Expr -> Store
+updateStore store a e1
+    | item == Nothing = insert a e1 store
+    | otherwise = update (\x -> Just e1) a store
+    where item = Map.lookup a store
