e5f642de972bd9c6f055668e17b809dd6bf2d081
jack-parsons <jack.parsons.uk@icloud.com>
2020-03-29 00:27:51 +0000

    Exit conditions

--- src/Preprocessor.hs
+++ src/Preprocessor.hs
@@ -5,6 +5,7 @@ import Data.Maybe
 -- import Data.List
 import Debug.Trace
 import System.IO (hPutStrLn, stderr)
+import System.Exit (exitFailure)
 
 type TStore = Map.Map String [Type]
 
@@ -15,15 +16,15 @@ preprocess e = do
     (t, _) <- process e (Map.empty, Map.empty)
     return ()
 
-process :: Expr -> ProcessState -> IO (Type, ProcessState)
+process :: Expr -> ProcessState -> IO ([Type], ProcessState)
 process (Seq e1 e2) (global, local) = do
     (t', (global', local')) <- process e1 (global, local)
     process e2 (global', local')
 
-process (Literal (EInt n)) (global, local) = return (TInt, (global, local))
-process (Literal (EBool b)) (global, local) = return (TBool, (global, local))
-process (Literal Empty) (global, local) = return (TList TEmpty, (global, local))
-process (Literal ENone) (global, local) = return (TNone, (global, local))
+process (Literal (EInt n)) (global, local) = return ([TInt], (global, local))
+process (Literal (EBool b)) (global, local) = return ([TBool], (global, local))
+process (Literal Empty) (global, local) = return ([TList TEmpty], (global, local))
+process (Literal ENone) (global, local) = return ([TNone], (global, local))
 
 --process (LocalAssign (DefVar s (FuncType ps out cs)), )
 --process (LocalAssign (DefVar s (Func ps e1)), )
@@ -31,31 +32,41 @@ process (Literal ENone) (global, local) = return (TNone, (global, local))
 process (LocalAssign (DefVar s e1)) (global, local) = do
     (t', (global', local')) <- process e1 (global, local)
     let lv = (lookupT s (global, local))
-    if lv /= Nothing && (fromJust lv) !! 0 /= t' -- Check type hasn't changed
-        then printStdErr ("WARNING: type changed: "++(show s))
+    if lv /= Nothing && (length (fromJust lv) /= 1 || (fromJust lv) /= t')
+        then printStdErr ("WARNING: ambiguous types for: "++(show s) ++ " in " ++ (show (LocalAssign (DefVar s e1))))
         else return ()
-    return (t', (global', Map.insert s [t'] local'))
+    return (t', (global', Map.insert s t' local'))
 
 process (Var s) state = do
     let t = (lookupT s state)
     if t == Nothing
-        then printStdErr ("WARNING: variable referenced before assignment: "++(show s))
+        then do
+            printStdErr ("ERROR: variable referenced before assignment: "++(show s))
+            exitFailure
         else return ()
-    return ((fromJust t)!!0, state)
+    return ((fromJust t), state)
 
 -- Make dodgy assumption that MathOps all return ints - don't judge me josh
 process (Op (MathOp op e1 e2)) (global, local)= do
     (t1, (global1, local1)) <- process e1 (global, local)
     (t2, (global2, local2)) <- process e2 (global1, local1)
-    if t1 /= TInt
-        then printStdErr ("WARNING: type invalid for op: "++(show e1))
+    if length t1 == 1 && t1!!0 /= TInt
+        then do
+            printStdErr ("ERROR: type invalid for op: "++(show (Op (MathOp op e1 e2))))
+            exitFailure
         else return ()
-    if t2 /= TInt
-        then printStdErr ("WARNING: type invalid for op: "++(show e2))
+    if length t2 == 1 && t2!!0 /= TInt
+        then do
+            printStdErr ("ERROR: type invalid for op: "++(show (Op (MathOp op e1 e2))))
+            exitFailure
         else return ()
-    return (TInt, (global2, local2))
+    return ([TInt], (global2, local2))
 
-process e _ = error (show e)
+-- TODO implement others
+process e s = return ([TNone], s)
+
+mergeTList :: [Type] -> [Type] -> [Type]
+mergeTList t1 t2 = [x | x <- t1, not (x `elem` t2)] ++ t2
 
 lookupT :: String -> ProcessState -> Maybe [Type]
 lookupT name (global, local)
