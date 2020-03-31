febe175687be470fb52c2dd870d5852541004caa
jack-parsons <jack.parsons.uk@icloud.com>
2020-03-31 16:49:06 +0100

    List type checking

--- src/Preprocessor.hs
+++ src/Preprocessor.hs
@@ -6,6 +6,7 @@ import Data.Maybe
 import Debug.Trace
 import System.IO (hPutStrLn, stderr)
 import System.Exit (exitFailure)
+import Evaluator
 
 type TStore = Map.Map String [Type]
 
@@ -46,7 +47,102 @@ process (Var s) state = do
         else return ()
     return ((fromJust t), state)
 
+
+process (Op (Cons e1 e2)) (global, local) = do
+    (t1, (global1, local1)) <- process e1 (global, local)
+    (t2, (global2, local2)) <- process e2 (global1, local1)
+    if (length t1 /= 1 || length t2 /= 1)
+        then do
+            printStdErr ("ERROR: Ambiguous types for: "++(show (Op (Cons e1 e2)))) -- Conflicting types error
+            exitFailure
+        else return()
+
+    let t' = consTypes (head t1) (head t2)
+
+    if (t' == TConflict)
+        then do
+            printStdErr ("ERROR: conflicting types for op: "++(show (Op (Cons e1 e2)))) -- Conflicting types error
+            exitFailure
+        else return()
+
+    return ([t'], (global, local))
+
+    where
+        consTypes e1 (TList e2)
+            | e1 == e2 = TList e1
+            | isEmptyList e2 = TList $ buildList e1 e2
+            where
+                buildList e1 TEmpty = e1
+                buildList e1 (TList e2) = TList (buildList e1 e2)
+
+                isEmptyList TEmpty = True
+                isEmptyList (TList e1) = isEmptyList e1
+                isEmptyList _ = False
+
+        consTypes (TList e1) (TList e2) = TList (consTypes e1 e2)
+        consTypes TEmpty e@(TList e2) = e
+
+        consTypes _ _ = TConflict
+
+
 -- Make dodgy assumption that MathOps all return ints - don't judge me josh
+process (Op (MathOp Plus e1 e2)) (global, local) = do
+    (t1, (global1, local1)) <- process e1 (global, local)
+    (t2, (global2, local2)) <- process e2 (global1, local1)
+    printStdErr ("T1:" ++ (show t1))
+    printStdErr ("T2:" ++ (show t2))
+
+    case length t1 == 1 of
+        True  -> do
+            case head t1 of
+                TInt    -> do
+                    case head t2 of
+                        TInt -> return ([TInt], (global2, local2))
+                        _    -> do
+                            printStdErr ("ERROR: conflicting types for op: "++(show (Op (MathOp Plus e1 e2)))) -- Conflicting types error
+                            exitFailure
+
+                TList t -> do
+                    case head t2 of
+                        TList t' -> do
+                            case joinListTypes (TList t) (TList t') of
+                                TConflict -> do
+                                    printStdErr ("ERROR: conflicting types for op: "++(show (Op (MathOp Plus e1 e2)))) -- Conflicting types error
+                                    exitFailure
+
+                                newT -> return([newT], (global2, local2))
+
+                            where
+                                joinListTypes TInt TInt = TInt
+                                joinListTypes TBool TBool = TBool
+                                joinListTypes t TEmpty = t
+                                joinListTypes TEmpty t = t
+                                joinListTypes TNone TNone = TNone
+                                joinListTypes (TRef t) (TRef t')
+                                    | newT /= TConflict = TRef newT
+                                    | otherwise = TConflict
+                                    where
+                                        newT = joinListTypes t t'
+
+                                joinListTypes (TList t) (TList t')
+                                    | newT /= TConflict = TList newT
+                                    | otherwise = TConflict
+                                    where
+                                        newT = joinListTypes t t'
+
+                                joinListTypes _ _ = TConflict
+
+                        _        -> do
+                            printStdErr ("ERROR: conflicting types for op: "++(show (Op (MathOp Plus e1 e2)))) -- Conflicting types error
+                            exitFailure
+                _       -> do
+                    printStdErr ("ERROR: conflicting types for op: "++(show (Op (MathOp Plus e1 e2)))) -- Conflicting types error
+                    exitFailure
+
+        False -> do
+            printStdErr ("ERROR: conflicting types for op: "++(show (Op (MathOp Plus e1 e2)))) -- Conflicting types error
+            exitFailure
+
 process (Op (MathOp op e1 e2)) (global, local)= do
     (t1, (global1, local1)) <- process e1 (global, local)
     (t2, (global2, local2)) <- process e2 (global1, local1)
@@ -61,6 +157,7 @@ process (Op (MathOp op e1 e2)) (global, local)= do
             exitFailure
         else return ()
     return ([TInt], (global2, local2))
+
 
 -- TODO implement others
 process e s = return ([TNone], s)
