b30513f5ca60b29e6c13eed2e8fc1abfbbaccfda
Josh Hook <jljh1g18@soton.ac.uk>
2020-04-17 11:32:49 +0100

    -

--- Example Files/Challenge 1/chl1HaskellSolution.hs
+++ Example Files/Challenge 1/chl1HaskellSolution.hs
@@ -8,13 +8,12 @@ main = do
     content <- readFile "largeInput.txt"
     let line = foldr (\x acc -> map (read :: String -> Int) (words x) : acc) [] $ lines content
     solve (map (!!0) line) (map (!!1) line)
-    -- writeFile "exp.txt" $ foldr (\i acc -> (show i) ++ "\n" ++ acc) "" (solve (map (!!0) line) (map (!!1) line))
 
 printInt a = do
     t <- getTime
     putStrLn $ (show a) ++ " : " ++ (show t)
 
-solve :: [Int] -> [Int] -> IO ()
+solve :: [Int] -> [Int] -> [Int]
 solve [] bs = return ()
 solve [a] bs = printInt a
 solve (a:a':as) (b:bs) = do
